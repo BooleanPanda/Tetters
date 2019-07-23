9654f780bc8da79a7aa51134acd15aa52d18d67d
Egor Mozheiko <gopherbopher123@gmail.com>
2019-07-23 14:13:00 +0300

    Updates

---
+++ precache-manifest.a12c80fd3da28b5cd4ca379535709220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34f268e978d67f368bbec10bbe7660b",
+    "url": "/tetters/index.html"
+  },
+  {
+    "revision": "e9c9dae1e5e28d96ab2e",
+    "url": "/tetters/static/css/main.e61bed15.chunk.css"
+  },
+  {
+    "revision": "a0a1d68a0d6063f35fa2",
+    "url": "/tetters/static/js/2.1a42d3c6.chunk.js"
+  },
+  {
+    "revision": "e9c9dae1e5e28d96ab2e",
+    "url": "/tetters/static/js/main.f733f0f9.chunk.js"
+  },
+  {
+    "revision": "7ceae307d6fac5ee79b6",
+    "url": "/tetters/static/js/runtime~main.cb3b1a57.js"
+  },
+  {
+    "revision": "33e43412f0889ca9f5064d050ed1887a",
+    "url": "/tetters/static/media/eye.33e43412.svg"
+  }
+]);
